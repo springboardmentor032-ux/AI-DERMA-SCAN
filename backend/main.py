153010a7c18ef546b359ccb1dabbca509be53216
src-sani <saneeshsanthosh0811@gmail.com>
2026-03-24 23:43:02 +0530

    add file upload endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi import UploadFile, File
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Backend running 🚀"}
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "type": file.content_type
+    }
